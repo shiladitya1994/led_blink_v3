12e3b640b56805be73c1b85d5b2c02a8b6323d54
shiladitya1994 <ghosh.shiladitya@mahindra.com>
2026-01-30 09:31:05 +0000

    blink200ms

--- main/blink_example_main.c
+++ main/blink_example_main.c
@@ -99,6 +99,6 @@ void app_main(void)
         blink_led();
         /* Toggle the LED state */
         s_led_state = !s_led_state;
-        vTaskDelay(100 / portTICK_PERIOD_MS);
+        vTaskDelay(200 / portTICK_PERIOD_MS);
     }
 }
